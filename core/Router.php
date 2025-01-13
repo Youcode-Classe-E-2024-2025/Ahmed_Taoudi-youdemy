db2fa0f6d59d23dc5a21456d6d44a7bffc002413
Tawdi <ahmedtawdi00@gmail.com>
2025-01-13 10:37:39 +0100

    ajoute de class router

---
+++ core/Router.php
@@ -0,0 +1,38 @@
+<?php
+
+class Router {
+    private $routes = [];
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+
+    public function register($route, $controller, $action) {
+        $this->routes[$route] = [
+            'controller' => $controller,
+            'action' => $action
+        ];
+    }
+
+
+    public function resolve($requestUri) {
+        
+        $path = parse_url($requestUri)['path'];
+        
+        if (isset($this->routes[$path])) {
+            $controller = $this->routes[$path]['controller'];
+            $action = $this->routes[$path]['action'];
+            $controllerPath = "controllers/{$controller}.php";
+            // dd($controllerPath);
+            require_once $controllerPath;
+            $controller = new $controller($this->db);
+            return $controller->$action();
+        }else{
+            return require_once "views/errors/404.php";
+        }
+        
+
+    }
+}
