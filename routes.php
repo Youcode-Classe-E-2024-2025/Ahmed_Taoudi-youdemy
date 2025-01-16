ad8250953b2286f98edcde7eb97625d67448b419
Tawdi <ahmedtawdi00@gmail.com>
2025-01-16 01:38:19 +0100

    ajoute des routes

--- routes.php
+++ routes.php
@@ -3,3 +3,16 @@
 $router->register('/login', 'AuthController', 'login');
 $router->register('/logout', 'AuthController', 'logout');
 $router->register('/register', 'AuthController', 'register');
+
+// dashboard 
+$router->register('/admin/dashboard', 'AdminController', 'index');
+
+$router->register('/teacher/dashboard', 'TeacherController', 'index');
+
+$router->register('/student/dashboard', 'StudentController', 'index');
+
+
+$router->register('/', 'HomeController', 'index');
+$router->register('/courses', 'HomeController', 'courses');
+
+
