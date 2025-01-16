23e137968dec806db96f15a086fbbbfe2d61bd8f
Tawdi <ahmedtawdi00@gmail.com>
2025-01-16 01:45:00 +0100

    ajoute de class user getter setter create function

---
+++ app/models/User.php
@@ -0,0 +1,110 @@
+<?php
+
+require_once "app/enums/Role.php";
+require_once "app/enums/UserStatus.php";
+
+class User
+{
+    protected $conn;
+    protected $table = 'users';
+
+    protected $id;
+    protected $name;
+    protected $email;
+    protected $password;
+    protected UserStatus $status;
+    protected Role $role;
+    protected $created_at;
+
+    public function __construct()
+    {
+        $this->conn = Database::getInstance();
+    }
+
+
+
+    // Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    public function getStatus(): UserStatus
+    {
+        return $this->status;
+    }
+
+    public function getRole(): Role
+    {
+        return $this->role;
+    }
+
+    // Setters
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function setStatus(UserStatus $status): void
+    {
+        $this->status = $status;
+    }
+
+    public function setRole(Role $role): void
+    {
+        $this->role = $role;
+    }
+
+
+    //create
+    public function create(): bool
+    {
+        $query = "INSERT INTO " . $this->table . " 
+                  (name, email, password, status, role, created_at)
+                  VALUES 
+                  (:name, :email, :password, :status, :role, NOW())";
+
+        $params = [
+            ':name' => $this->name,
+            ':email' => $this->email,
+            ':password' => $this->password,
+            ':status' => $this->status->value,
+            ':role' => $this->role->value
+        ];
+
+        try {
+            $this->conn->query($query, $params);
+            return true;
+        } catch (PDOException $e) {
+            error_log("Error creating user: " . $e->getMessage());
+            return false;
+        }
+    }
+
+}
