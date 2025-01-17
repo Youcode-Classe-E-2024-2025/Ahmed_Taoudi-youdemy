b0471d5c87bfec59879ede9f043a1ea0800607af
Tawdi <ahmedtawdi00@gmail.com>
2025-01-17 10:07:36 +0100

    ajoute index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+require_once "core/Debug.php";
+require_once "core/Router.php";
+require_once "core/Database.php";
+require_once "core/Security.php";
+require_once 'core/Validator.php';
+
+require_once 'app/controllers/BaseController.php';
+
+require_once "app/enums/Role.php";
+require_once "app/enums/UserStatus.php";
+
+require_once "app/models/Course.php";
+require_once "app/models/Teacher.php";
+require_once "app/models/Category.php";
+require_once "app/models/Tag.php";
+require_once "app/models/User.php";
+
+
+// CSRF
+Security::generateCSRFToken();
+// $password="Password123";
+// Debug::dd(password_hash($password, PASSWORD_BCRYPT));
+// $db = Database::getInstance();
+// 
+
+if(isset($_POST['createdb'])){ 
+
+    if(isset($_POST['checkbox']) && $_POST['checkbox'] == 'on' ){
+        Database::createDatabase(DBNAME,true);
+        // dd(33);
+    }else{
+        Database::createDatabase(DBNAME,false);
+        // dd(22);
+    }
+    
+}
+
+$router = new Router();
+
+require "routes.php";
+
+$router->resolve($_SERVER['REQUEST_URI']);
