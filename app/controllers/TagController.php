3609c4cdb9bebeff87f402f034d14a26f972b127
Tawdi <ahmedtawdi00@gmail.com>
2025-01-19 12:24:22 +0100

    ajoute des tags (admin)

---
+++ app/controllers/TagController.php
@@ -0,0 +1,50 @@
+<?php
+
+class TagController extends BaseController
+{
+    private Tag $tagModel;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->tagModel = new Tag();
+    }
+
+    public function create()
+    {
+        $this->requireRole(Role::ADMIN);
+        if ($this->isPost()) {
+            // Verify CSRF token
+            $token = $_POST['csrf_token'] ?? '';
+            if (!Security::verifyCSRFToken($token)) {
+                $this->setFlashMessage('error', 'CSRF token validation failed. Possible CSRF attack.');
+                Security::regenerateCSRFToken();
+                $this->redirect('/admin/tags');
+            }
+            // Debug::dd($_POST['tags'] );
+            $validator = new Validator();
+            // $a='';
+            // $validator->validateName($a) ;
+            $validator->validateArrayOfString($_POST['tags'], 'tous les champs de catégorie');
+            if ($validator->isValid()) {
+                $tags = [];
+                foreach ($_POST['tags'] as $tg) {
+                    $tags[] = new Tag($tg);
+                }
+                // Debug::pd($tags);
+                if ($this->tagModel->create($tags)) {
+
+                    $this->setFlashMessage('message', 'tags ajouté avec succès.');
+                } else {
+
+                    $this->setFlashMessage('error', ' ajoute de tags a échoué. Veuillez réessayer .');
+                }
+            } else {
+                $_SESSION['errors'] = $validator->getErrors();
+            }
+            $this->redirect('/admin/tags');
+        } else {
+            $this->_404();
+        }
+    }
+}
