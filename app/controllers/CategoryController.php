5a5133ef95c1c6c6aa8b8bb564cc55f65bc4a450
Tawdi <ahmedtawdi00@gmail.com>
2025-01-19 11:40:37 +0100

    ajoute des category (admin)

---
+++ app/controllers/CategoryController.php
@@ -0,0 +1,51 @@
+<?php
+
+class CategoryController extends BaseController
+{
+    private Category $categoryModel;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->categoryModel = new Category();
+    }
+
+    public function create()
+    {
+
+        // Handle POST request
+        if ($this->isPost()) {
+            // Verify CSRF token
+            $token = $_POST['csrf_token'] ?? '';
+            if (!Security::verifyCSRFToken($token)) {
+                $this->setFlashMessage('error', 'CSRF token validation failed. Possible CSRF attack.');
+                Security::regenerateCSRFToken();
+                $this->redirect('/admin/categories');
+            }
+            // Debug::dd($_POST['categories'] );
+            $validator = new Validator();
+            // $a='';
+            // $validator->validateName($a) ;
+            $validator->validateArrayOfString($_POST['categories'], 'tous les champs de catégorie');
+            if ($validator->isValid()) {
+                $categorys = [];
+                foreach ($_POST['categories'] as $ctg) {
+                    $categorys[] = new Category($ctg);
+                }
+                // Debug::pd($categorys);
+                if ($this->categoryModel->create($categorys)) {
+
+                    $this->setFlashMessage('message', 'categorys ajouté avec succès.');
+                } else {
+
+                    $this->setFlashMessage('error', ' ajoute de categorys a échoué. Veuillez réessayer .');
+                }
+            } else {
+                $_SESSION['errors'] = $validator->getErrors();
+            }
+            $this->redirect('/admin/categories');
+        } else {
+            $this->_404();
+        }
+    }
+}
