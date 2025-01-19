b0599e98c8b83162a1f1e1664352ab75258db01b
Tawdi <ahmedtawdi00@gmail.com>
2025-01-19 21:14:31 +0100

    ajoute nav-bar for for user

---
+++ app/views/partials/dashboard-nav.php
@@ -0,0 +1,21 @@
+<!-- Navbar -->
+<nav class="fixed top-0 left-0 right-0 z-30 flex items-center justify-between h-16 px-4 bg-white border-b">
+        <!-- Left Section: Application Name and Burger Menu -->
+        <div class="flex items-center space-x-4">
+            <!-- Burger Menu -->
+            <button id="burgerMenu" class="lg:hidden p-2 rounded-lg hover:bg-gray-100">
+                <i class="ri-menu-line text-xl text-gray-500"></i>
+            </button>
+            <!-- Application Name -->
+            <h1 class="text-2xl font-bold text-emerald-600">Youdemy</h1>
+        </div>
+
+        <!-- Right Section: Logout Button -->
+        <div>
+            <a href="/logout" class="flex items-center p-2 text-gray-500 rounded-lg hover:bg-gray-100">
+                <i class="ri-logout-box-r-line text-xl"></i>
+                <span class="ml-2">Déconnexion</span>
+            </a>
+        </div>
+    </nav>
+    
