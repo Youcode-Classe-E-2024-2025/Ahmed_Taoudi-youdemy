632fc9aaa00d0869c46bbeceaee1fec752ded3ea
Tawdi <ahmedtawdi00@gmail.com>
2025-01-19 21:34:47 +0100

    ajoute des fonctions implementer au student class

--- app/models/Student.php
+++ app/models/Student.php
@@ -1,10 +1,42 @@
-<?php 
+<?php
 
 require_once "app/models/User.php";
 
 // Student Class
-class Student extends User
+class Student extends User implements UserCourse
 {
 
+    private array $courses = [];
 
+    public function getMyCourses($limit = 0, $offset = 0)
+    {
+
+        $query = "SELECT c.*, f.id AS image_id, f.name AS image_name, f.path AS image_path 
+                      FROM courses c
+                      JOIN student_course sc ON c.id = sc.course_id
+                      LEFT JOIN course_files cf ON c.id = cf.course_id
+                      LEFT JOIN files f ON cf.file_id = f.id AND f.file_type_id = (SELECT id FROM file_types WHERE name = 'photo') 
+                      WHERE sc.student_id = :student_id
+                      ORDER BY c.created_at DESC";
+        $params = ['student_id' => $this->getId()];
+
+        $results = $this->conn->pagination($query, $params, $limit, $offset)->fetchAll();
+        $courses = [];
+        foreach ($results as $row) {
+            $course = new Course();
+            $courses[] = $course->createObject($row);
+        }
+
+        return $courses;
+        return $this->courses;
+    }
+    public function getCount()
+    {
+        $query = "SELECT COUNT(*) 
+                 FROM  student_course sc 
+                 WHERE  sc.student_id = :student_id ";
+
+        $params['student_id'] = $this->getId();
+        return $this->conn->query($query, $params)->fetchColumn();
+    }
 }
